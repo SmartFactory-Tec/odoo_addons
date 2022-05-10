b1dc3b3fc2d223c7564fea8b8c7a3ac2fbb9611d
Hiram Munoz <hirammunozr@outlook.com>
2022-05-10 14:22:22 -0500

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+        'name': "Smart Factory",
+        'version': '1.0',
+        'author': 'Hiram Muñoz',
+        'category': 'Manufacturing/Tec',
+        'application': True,
+        'depends': [
+            'base'
+        ],
+        'data': [
+        ],
+}
